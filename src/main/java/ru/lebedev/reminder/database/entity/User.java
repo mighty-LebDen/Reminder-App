88de77b446b61f5c5718e967167b99492809d656
mighty-LebDen <lebedev.dm00@mail.ru>
2024-09-05 19:47:47 +0900

    add empty lines between fields

--- src/main/java/ru/lebedev/reminder/database/entity/User.java
+++ src/main/java/ru/lebedev/reminder/database/entity/User.java
@@ -24,8 +24,13 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String username;
+	
 	private String password;
+	
+	private String telegram;
+	
 	@OneToMany(mappedBy = "user", orphanRemoval = true)
 	@Builder.Default
 	private List<Reminder> reminders = new ArrayList<>();
