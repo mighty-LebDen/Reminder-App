344c76c61a761fc0c7c5d67b93c33aa193f77620
mighty-LebDen <lebedev.dm00@mail.ru>
2024-09-05 19:31:50 +0900

    add User Service

--- src/main/java/ru/lebedev/reminder/service/UserService.java
+++ src/main/java/ru/lebedev/reminder/service/UserService.java
@@ -1,7 +1,9 @@
 package ru.lebedev.reminder.service;
 
 import ru.lebedev.reminder.database.entity.User;
+import ru.lebedev.reminder.dto.UserReadDto;
 
 public interface UserService {
 	User findById(Long id);
+	UserReadDto create(UserCreateEditDto dto);
 }
