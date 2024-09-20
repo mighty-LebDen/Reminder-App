953f481afbc1e7b35a8270ecccc11983051e83a8
mighty-LebDen <lebedev.dm00@mail.ru>
2024-09-20 19:42:39 +0900

    fix(тесты напишу позже)

--- src/test/java/unit/mapper/UserMapperHelperTest.java
+++ src/test/java/unit/mapper/UserMapperHelperTest.java
@@ -1,11 +1,7 @@
 package unit.mapper;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import ru.lebedev.reminder.mapper.UserMapperHelper;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class UserMapperHelperTest {
 @Test
