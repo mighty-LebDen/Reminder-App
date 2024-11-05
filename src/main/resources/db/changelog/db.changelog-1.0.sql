4252c588d259a45de079b9a0ba0b5131cd91f96b
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:01:03 +0900

    Добавлена миграция для 2 таблиц

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS remainders
     id          BIGSERIAL PRIMARY KEY,
     title       varchar(255)  NOT NULL,
     description varchar(4096) NOT NULL,
-    remind      date,
-    user_id     bigint references users (id) ON DELETE CASCADE ON UPDATE CASCADE
+    remind      timestamp,
+    user_id     bigint references users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    is_sent     varchar(10) not null default 'NOT_SENT'
 );
