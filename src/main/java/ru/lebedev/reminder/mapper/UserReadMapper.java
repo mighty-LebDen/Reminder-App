db8e4f8de164ad7d57c96cdf43fc4d95dea2c63e
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:08:02 +0900

    исправления

--- src/main/java/ru/lebedev/reminder/mapper/UserReadMapper.java
+++ src/main/java/ru/lebedev/reminder/mapper/UserReadMapper.java
@@ -2,10 +2,11 @@ package ru.lebedev.reminder.mapper;
 
 import org.mapstruct.Mapper;
 
+import org.mapstruct.MappingConstants;
 import ru.lebedev.reminder.database.entity.User;
 import ru.lebedev.reminder.dto.UserReadDto;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserReadMapper {
 	UserReadDto toDto(User user);
 }
