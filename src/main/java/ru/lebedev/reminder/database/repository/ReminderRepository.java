49ee26d46124c1adf99b73b3f644f5702f474a01
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:11:18 +0900

    Добавлены методы для поиска напоминания для отправки, и обновления статуса напоминания

--- src/main/java/ru/lebedev/reminder/database/repository/ReminderRepository.java
+++ src/main/java/ru/lebedev/reminder/database/repository/ReminderRepository.java
@@ -1,16 +1,26 @@
 package ru.lebedev.reminder.database.repository;
 
-import org.springframework.data.domain.Sort;
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import ru.lebedev.reminder.database.entity.Reminder;
+import ru.lebedev.reminder.database.entity.SentStatus;
 
 public interface ReminderRepository extends
         JpaRepository<Reminder, Long>,
         ReminderCustomRepository,
         QuerydslPredicateExecutor<Reminder> {
 
-    List<Reminder> findAllBy(Sort sort);
+    @Query("select r from Reminder r where r.status = 'NOT_SENT' AND r.remind <= :now")
+    List<Reminder> findReminderByRemindBefore(@Param("now") LocalDateTime now);
+
+    @Modifying
+    @Query("UPDATE Reminder r SET r.status = :status WHERE r.id = :id")
+    void updateStatus(@Param("id") Long id, @Param("status") SentStatus status);
+
 
 }
