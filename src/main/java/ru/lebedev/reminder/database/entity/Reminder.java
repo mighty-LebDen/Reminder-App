3cb9c78b0e36af51fd59b8bb704ff6da1aa8e48c
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:02:42 +0900

    Добавлено поле для отслеживания состояния отправки напоминания

--- src/main/java/ru/lebedev/reminder/database/entity/Reminder.java
+++ src/main/java/ru/lebedev/reminder/database/entity/Reminder.java
@@ -1,7 +1,7 @@
 package ru.lebedev.reminder.database.entity;
 
-import java.time.LocalDate;
-
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,32 +11,40 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "user")
 @Builder
 @Entity
 @Table(name = "remainder")
 public class Reminder {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable = false)
-	private String title;
-	
-	@Column(nullable = false)
-	private String description;
-	
-	private LocalDate remind;
-	
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "user_id")
-	private User user;
-	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private LocalDateTime remind;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "is_sent")
+    @Enumerated(EnumType.STRING)
+    private SentStatus status;
+
 }
