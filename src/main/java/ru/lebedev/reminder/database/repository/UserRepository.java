15d9752466da9dcfb456c9ecc62fa2d26688824c
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:07:46 +0900

    добавлены метод для поиска пользователя по username

--- src/main/java/ru/lebedev/reminder/database/repository/UserRepository.java
+++ src/main/java/ru/lebedev/reminder/database/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package ru.lebedev.reminder.database.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ru.lebedev.reminder.database.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
