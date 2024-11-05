563a0b6281bd68750fc5b3602eda03ac702f7300
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:04:38 +0900

    Доработаны мапперы

--- src/main/java/ru/lebedev/reminder/mapper/ReminderCreateMapper.java
+++ src/main/java/ru/lebedev/reminder/mapper/ReminderCreateMapper.java
@@ -3,21 +3,20 @@ package ru.lebedev.reminder.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.springframework.stereotype.Component;
 
 import ru.lebedev.reminder.database.entity.Reminder;
-import ru.lebedev.reminder.dto.ReminderCreateDto;
-import ru.lebedev.reminder.service.UserService;
-import ru.lebedev.reminder.service.impl.UserServiceImpl;
+import ru.lebedev.reminder.dto.ReminderCreateEditDto;
 
 @Mapper
 public interface ReminderCreateMapper {
 
     @Mapping(target = "id", ignore = true)
-    Reminder reminderCreateDtoToReminder(ReminderCreateDto dto);
+    Reminder reminderCreateDtoToReminder(ReminderCreateEditDto dto);
 
     @Mapping(target = "id", ignore = true)
-    void updateEntity(ReminderCreateDto dto, @MappingTarget Reminder reminder);
+    void updateEntity(ReminderCreateEditDto dto, @MappingTarget Reminder reminder);
+
+
 
 }
 
--- src/main/java/ru/lebedev/reminder/mapper/ReminderReadMapper.java
+++ src/main/java/ru/lebedev/reminder/mapper/ReminderReadMapper.java
@@ -2,9 +2,14 @@ package ru.lebedev.reminder.mapper;
 
 import org.mapstruct.Mapper;
 
+import org.mapstruct.Mapping;
 import ru.lebedev.reminder.database.entity.Reminder;
 import ru.lebedev.reminder.dto.ReminderReadDto;
+
 @Mapper
 public interface ReminderReadMapper {
-	ReminderReadDto reminderToReminderReadDto(Reminder reminder);
+
+    @Mapping(target = "userReadDto", source = "user")
+    ReminderReadDto reminderToReminderReadDto(Reminder reminder);
+
 }
