86f2955d78ff16f85666f4de3a7cc6088d531907
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:03:11 +0900

    Добавлен Scheduling

--- src/main/java/ru/lebedev/reminder/ReminderApplication.java
+++ src/main/java/ru/lebedev/reminder/ReminderApplication.java
@@ -2,9 +2,10 @@ package ru.lebedev.reminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReminderApplication {
 	
 	public static void main(String[] args) {
