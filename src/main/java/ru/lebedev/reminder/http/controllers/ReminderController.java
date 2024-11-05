818fe21ccc26ea18accf015245f7788239226007
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-05 21:03:23 +0900

    Обновлены методы контроллера

--- src/main/java/ru/lebedev/reminder/http/controllers/ReminderController.java
+++ src/main/java/ru/lebedev/reminder/http/controllers/ReminderController.java
@@ -1,22 +1,26 @@
 package ru.lebedev.reminder.http.controllers;
 
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
-import ru.lebedev.reminder.dto.ReminderCreateDto;
+import ru.lebedev.reminder.dto.ReminderCreateEditDto;
 import ru.lebedev.reminder.dto.ReminderReadDto;
-import ru.lebedev.reminder.dto.ReminderUpdateRequest;
-import ru.lebedev.reminder.filters.Filter;
-import ru.lebedev.reminder.filters.ReminderSearchFilter;
+import ru.lebedev.reminder.exception.ReminderNotFoundException;
+import ru.lebedev.reminder.filters.DateAndTimeFilter;
+import ru.lebedev.reminder.filters.SearchFilter;
 import ru.lebedev.reminder.service.impl.ReminderService;
 
 @RestController
@@ -27,44 +31,48 @@ public class ReminderController {
     private final ReminderService reminderService;
 
     @PostMapping("/create")
-    public ReminderReadDto create(@RequestBody ReminderCreateDto dto) {
-        System.out.println(dto);
-        return Optional.ofNullable(reminderService.create(dto)).orElseThrow(() -> new RuntimeException("bad"));
+    public ReminderReadDto create(@Validated @RequestBody ReminderCreateEditDto dto) {
+        return Optional.ofNullable(reminderService.create(dto))
+                       .orElseThrow(() -> new RuntimeException("bad"));
     }
 
     @GetMapping("/list")
-    public List<ReminderReadDto> findAll(Integer total, Integer current) {
+    public List<ReminderReadDto> findAll(
+            @RequestParam(name = "total", defaultValue = "3") @Positive Integer total,
+            @RequestParam(name = "current", defaultValue = "0") Integer current) {
         return reminderService.findAllWithPagination(PageRequest.of(current, total));
     }
 
-//    @GetMapping("/id")
-//    public ReminderReadDto findById(Long id) {
-//        return reminderService.findById(2L);
-//    }
-
-    @PutMapping("/update")
-    public ReminderReadDto update(@RequestBody ReminderUpdateRequest request) {
-        var reminder = reminderService.update(request.id(), request.dto());
-        return reminder.orElseThrow(() -> new RuntimeException());
+    @PutMapping("/{id}/update")
+    public ReminderReadDto update(
+            @PathVariable @Positive Long id,
+            @Validated @RequestBody ReminderCreateEditDto request) {
+        var reminder = reminderService.update(id, request);
+        return reminder.orElseThrow(() -> new ReminderNotFoundException(id));
     }
 
-    @DeleteMapping("/delete")
-    public void delete(@RequestBody Long id) {
+    @DeleteMapping("/{id}/delete")
+    public void delete(@Positive @PathVariable Long id) {
         reminderService.delete(id);
     }
 
     @GetMapping("/find")
-    public List<ReminderReadDto> findBySearchFilter(ReminderSearchFilter filter) {
+    public List<ReminderReadDto> findBySearchFilter(
+            @Validated
+            @RequestBody SearchFilter filter) {
         return reminderService.findAllBySearchFilter(filter);
     }
+
     @GetMapping("/sort")
-    public List<ReminderReadDto> findAllBySort(String field) {
-        return reminderService.sort(field);
+    public List<ReminderReadDto> findAllBySort(
+            @RequestParam(name = "direction", defaultValue = "asc") String direction,
+            @RequestParam(name = "field", defaultValue = "id") String field) {
+        return reminderService.sort(direction, field);
     }
 
     @GetMapping("/filter")
-    public List<ReminderReadDto> findAllByFilter(Filter filter) {
-        return reminderService.findAllByFilter(filter);
+    public List<ReminderReadDto> findAllByFilter(@Validated @RequestBody DateAndTimeFilter filter) {
+        return reminderService.findAllByDateAndTimeFilter(filter);
     }
 
 }
