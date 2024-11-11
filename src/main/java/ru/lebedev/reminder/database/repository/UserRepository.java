17dc524f77aea43e5f78ae32094d4a55f504e1d4
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-11 14:58:16 +0900

    добавлен метод для поиска пользователя по tg

--- src/main/java/ru/lebedev/reminder/database/repository/UserRepository.java
+++ src/main/java/ru/lebedev/reminder/database/repository/UserRepository.java
@@ -2,10 +2,12 @@ package ru.lebedev.reminder.database.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import ru.lebedev.reminder.database.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByTelegram(String telegram);
+
 }
