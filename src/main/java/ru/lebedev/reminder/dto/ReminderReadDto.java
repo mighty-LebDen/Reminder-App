d22c0f1343abaf7d0c30871288c392bdb25882bf
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-11 14:52:17 +0900

    refactoring

--- src/main/java/ru/lebedev/reminder/dto/ReminderReadDto.java
+++ src/main/java/ru/lebedev/reminder/dto/ReminderReadDto.java
@@ -2,9 +2,10 @@ package ru.lebedev.reminder.dto;
 
 import java.time.LocalDateTime;
 
-public record ReminderReadDto(Long id,
-                              String title,
-                              String description,
-                              LocalDateTime remind,
-                              UserReadDto userReadDto
+public record ReminderReadDto(
+        Long id,
+        String title,
+        String description,
+        LocalDateTime remind,
+        UserReadDto userReadDto
 ) {}
