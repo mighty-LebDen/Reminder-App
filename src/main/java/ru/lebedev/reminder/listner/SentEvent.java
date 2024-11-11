5240dcef4f1e0a62fc0cd38c3ae5fa3ce9fec55d
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-11 14:57:04 +0900

    refactoring

--- src/main/java/ru/lebedev/reminder/listner/SentEvent.java
+++ src/main/java/ru/lebedev/reminder/listner/SentEvent.java
@@ -3,18 +3,19 @@ package ru.lebedev.reminder.listner;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 import ru.lebedev.reminder.database.entity.SentStatus;
-import ru.lebedev.reminder.dto.ReminderReadDto;
 
 @Getter
-public class EmailSentEvent extends ApplicationEvent {
+public class SentEvent extends ApplicationEvent {
 
-    private final ReminderReadDto reminder;
+    private final Long reminderId;
     private final SentStatus status;
+    private final SentType type;
 
-    public EmailSentEvent(Object source, ReminderReadDto reminder, SentStatus status) {
+    public SentEvent(Object source, Long reminderId, SentStatus status, SentType type) {
         super(source);
-        this.reminder = reminder;
+        this.reminderId = reminderId;
         this.status = status;
+        this.type = type;
     }
 
 }
