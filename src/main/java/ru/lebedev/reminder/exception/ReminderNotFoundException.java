5e2c27b6389044b099fd9702d828a659d13497f8
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-11 14:45:04 +0900

    Удалены не нужные компоненты

--- src/main/java/ru/lebedev/reminder/exception/ReminderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.lebedev.reminder.exception;
-
-public class ReminderNotFoundException extends RuntimeException {
-    public ReminderNotFoundException(Long id) {
-        super(String.format("Reminder with id %d not found", id));
-    }
-}
