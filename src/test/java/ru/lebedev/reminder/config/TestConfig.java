770dd672cc6ebd335901bd0bf22694d41b1b2dd8
mighty-LebDen <lebedev.dm00@mail.ru>
2024-11-18 19:52:05 +0900

    refactoring

--- src/test/java/ru/lebedev/reminder/config/TestConfig.java
+++ src/test/java/ru/lebedev/reminder/config/TestConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +20,7 @@ public class TestConfig {
 
     @Bean
     public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authorizeRequests ->
                                            authorizeRequests
                                                    .anyRequest()
